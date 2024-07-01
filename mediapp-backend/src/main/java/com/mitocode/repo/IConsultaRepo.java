286801566d2e4fad5fb5887a7a98ea75086ad1cc
rlaricoinf <rlaricoinf@gmail.com>
2024-07-01 15:19:15 -0400

    tercer commit

---
+++ mediapp-backend/src/main/java/com/mitocode/repo/IConsultaRepo.java
@@ -0,0 +1,8 @@
+package com.mitocode.repo;
+
+import com.mitocode.model.Consulta;
+
+//@Repository
+public interface IConsultaRepo extends IGenericRepo<Consulta, Integer>{
+
+}
---
+++ mediapp-backend/src/main/java/com/mitocode/service/impl/ConsultaServiceImpl.java
@@ -0,0 +1,46 @@
+package com.mitocode.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mitocode.dto.ConsultaListaExamenDTO;
+import com.mitocode.model.Consulta;
+import com.mitocode.repo.IConsultaExamenRepo;
+import com.mitocode.repo.IConsultaRepo;
+import com.mitocode.repo.IGenericRepo;
+import com.mitocode.service.IConsultaService;
+
+@Service
+public class ConsultaServiceImpl extends CRUDImpl<Consulta, Integer> implements IConsultaService {
+
+	@Autowired
+	private IConsultaRepo repo;
+	
+	@Autowired
+	private IConsultaExamenRepo ceRepo;
+
+	@Override
+	protected IGenericRepo<Consulta, Integer> getRepo() {
+		return repo;
+	}
+
+	@Transactional
+	@Override
+	public Consulta registrarTransaccional(ConsultaListaExamenDTO dto) {
+		
+		/*for(DetalleConsulta det : consulta.getDetalleConsulta()) {
+			det.setConsulta(consulta);
+		}*/
+		dto.getConsulta().getDetalleConsulta().forEach(det -> det.setConsulta(dto.getConsulta()));
+		
+		repo.save(dto.getConsulta());
+		
+		dto.getLstExamen().forEach(ex -> ceRepo.registrar(dto.getConsulta().getIdConsulta(), ex.getIdExamen()));
+		
+		return dto.getConsulta();
+	}
+	
+	
+
+}
